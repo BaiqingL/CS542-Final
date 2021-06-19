ac87a2985f8fe7470b76ec048fd61e2585ce3572
Baiqing Lyu <baiqing@bu.edu>
2021-06-19 00:50:51 -0400

    MVP for cleaning data

--- src/data_processing.py
+++ src/data_processing.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 world_happiness = pd.read_csv ('../data/world-happiness-report.csv')
 world_happiness_shape = world_happiness.shape
@@ -10,6 +11,7 @@ world_happiness_shape = world_happiness.shape
 
 world_happiness_2021 = pd.read_csv ('../data/world-happiness-report-2021.csv')
 world_happiness_2021_shape = world_happiness_2021.shape
+
 # print(world_happiness_2021)
 
 # there are 373 null values in world_happiness and we currently find which country the null value belongs to 
@@ -17,36 +19,13 @@ world_happiness_2021_shape = world_happiness_2021.shape
 
 # Get a sample row from the dataset
 
-world_happiness_dict = {}
+world_happiness_dict = defaultdict()
 for row in range(world_happiness_shape[0]):
     value = world_happiness.loc[row].tolist()
-    print(value)
     keys = world_happiness_dict.keys()
     current_country = world_happiness.loc[row][0]
     if (current_country in world_happiness_dict.keys()):
-        print(current_country)
-        world_happiness_dict[current_country] = world_happiness_dict.get(current_country).append(value)
+        world_happiness_dict[current_country].append(value[1:])
     else:
-        world_happiness_dict[world_happiness.loc[row][0]] = value
-print(world_happiness_dict.get("Afghanistan"))
-"""
-
-for element in world_happiness:
-    i = 1
-    country_array = []
-    country_data_array = []
-    temp_data = []
-    for country in world_happiness.iloc[0][1:]:
-        print(country)
-        if country not in country_array:
-            country_array.append(country)
-            country_data_array.append(temp_data)
-            temp_data = []
-            temp_data.append(world_happiness.loc[i])
-            i+=1
-        else:
-            temp_data.append(world_happiness.loc[i])
-            i+=1
-print(country_array)
-
-"""
+        world_happiness_dict[current_country] = value
+print(np.isnan(world_happiness_dict.get("Bahrain")[14][6]))
